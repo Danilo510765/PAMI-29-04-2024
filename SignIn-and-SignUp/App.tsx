5a87a223a58c3d8cabffda3f6377f338781a70ef
Danilo510765 <danilobakana2004@gmail.com>
2024-05-20 19:55:40 -0300

    segunda parte projeto PAMI

--- SignIn-and-SignUp/App.tsx
+++ SignIn-and-SignUp/App.tsx
@@ -1,8 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { SignIn } from './Pages/SignIn';
+import { Routes } from "./src/routes";
 
 export default function App() {
   return (
-  <SignIn/>
+    <NavigationContainer>
+  <Routes/>
+  </NavigationContainer>
   );
 }
